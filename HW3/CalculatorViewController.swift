b0910c7720345c0048960c5eabe3f0550ee563c8
Allison Bickford <abickford@michiganlabs.com>
2019-09-23 13:27:41 -0400

    created general format

--- HW3/CalculatorViewController.swift
+++ HW3/CalculatorViewController.swift
@@ -9,13 +9,44 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
+    @IBOutlet weak var fromLabel: UILabel!
+    @IBOutlet weak var toLabel: UILabel!
     @IBOutlet weak var fromUnits: UILabel!
-    @IBOutlet weak var toUnites: UILabel!
+    @IBOutlet weak var toUnits: UILabel!
+    @IBOutlet weak var fromField: UITextField!
+    @IBOutlet weak var toField: UITextField!
     @IBOutlet weak var calcButton: UIButton!
+    
     @IBOutlet weak var clearButton: UIButton!
     @IBOutlet weak var modeButton: UIButton!
     
     var fUnits: String?
     var tUnits: String?
+    
+    @IBAction func calculate(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func clear(_ sender: UIButton) {
+        fromField.text = ""
+        toField.text = ""
+    }
+    
+    @IBAction func mode(_ sender: UIButton) {
+        // TODO
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        fromField.placeholder = "Enter length in Yards"
+        fromField.clearButtonMode = UITextField.ViewMode.whileEditing
+        toField.placeholder = "Enter length in Meters"
+        toField.clearButtonMode = UITextField.ViewMode.whileEditing
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 
 }
