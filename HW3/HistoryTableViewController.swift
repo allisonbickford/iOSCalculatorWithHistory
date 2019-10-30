d4a42795b86dcb63cad4c176a3309a1902dd82b4
Allison Bickford <abickford@michiganlabs.com>
2019-10-30 13:48:23 -0400

    step 4

--- HW3/HistoryTableViewController.swift
+++ HW3/HistoryTableViewController.swift
@@ -13,6 +13,11 @@ class HistoryTableViewController: UITableViewController {
     var entries: [Conversion] = []
     var historyDelegate:HistoryTableViewControllerDelegate?
     
+    override func viewDidLoad() {
+       super.viewDidLoad()
+       self.sortIntoSections(entries: self.entries)
+    }
+    
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
@@ -41,9 +46,77 @@ class HistoryTableViewController: UITableViewController {
        
        // this pops back to the main calculator
        _ = self.navigationController?.popViewController(animated: true)
+    }
+
+    var tableViewData: [(sectionHeader: String, entries: [Conversion])]? {
+       didSet {
+           DispatchQueue.main.async {
+               self.tableView.reloadData()
+           }
+       }
+    }
+
+   func sortIntoSections(entries: [Conversion]) {
+       
+       var tmpEntries : Dictionary<String,[Conversion]> = [:]
+       var tmpData: [(sectionHeader: String, entries: [Conversion])] = []
+       
+       // partition into sections
+       for entry in entries {
+        let shortDate = entry.timestamp.short
+           if var bucket = tmpEntries[shortDate] {
+               bucket.append(entry)
+               tmpEntries[shortDate] = bucket
+           } else {
+               tmpEntries[shortDate] = [entry]
+           }
+       }
+       
+       // breakout into our preferred array format
+       let keys = tmpEntries.keys
+       for key in keys {
+           if let val = tmpEntries[key] {
+               tmpData.append((sectionHeader: key, entries: val))
+           }
+       }
+
+       // sort by increasing date.
+       tmpData.sort { (v1, v2) -> Bool in
+           if v1.sectionHeader < v2.sectionHeader {
+               return true
+           } else {
+               return false
+           }
+       }
+       
+       self.tableViewData = tmpData
    }
+
 }
 
 protocol HistoryTableViewControllerDelegate {
     func selectEntry(entry: Conversion)
 }
+
+extension Double {
+    /// Rounds the double to decimal places value
+    func roundTo(places:Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+    }
+}
+
+extension Date {
+    struct Formatter {
+        static let short: DateFormatter = {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd"
+            return formatter
+        }()
+    }
+    
+    var short: String {
+        return Formatter.short.string(from: self)
+    }
+}
+
